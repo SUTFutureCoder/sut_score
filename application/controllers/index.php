79ff1c7ebeaea6759f3e7a6251bb570eb4a1e0e8
linxingchen <linxingchen@baidu.com>
2015-05-06 16:48:48 +0800

    FIN fetch cookies

--- application/controllers/index.php
+++ application/controllers/index.php
@@ -31,223 +31,81 @@ class Index extends CI_Controller{
     {          
         $this->load->library('session');
         
-        $this->load->view('index_view');
+        $this->load->view('login_view');
     }
+    
+    //显示验证码
+    public function getAgnomen(){
+        $this->load->library('session');
+        $url = 'http://jwc.sut.edu.cn/ACTIONVALIDATERANDOMPICTURE.APPPROCESS';
+        
+        $ch_cookie = curl_init();
+        curl_setopt($ch_cookie, CURLOPT_URL, $url);
+        curl_setopt($ch_cookie, CURLOPT_HEADER, 1); 
+        curl_setopt($ch_cookie, CURLOPT_RETURNTRANSFER, 1);
+        $content = curl_exec($ch_cookie);
+        curl_close($ch_cookie);
+        
+        list($header, $body) = explode("\r\n\r\n", $content);
+        preg_match("/set\-cookie:([^\r\n!]*)/i", $header, $matches); 
+        $cookie = $matches[1];  
+        
+        $this->session->set_userdata('cookie', $cookie);
+        header("Content-Type:image/jpg");
+        echo ($body);
+    }
+    
     
     
     /**    
      *  @Purpose:    
      *  验证用户登陆    
      *  @Method Name:
-     *  checkUserLogin()    
+     *  PassCheck()    
      *  @Parameter: 
      *  
      *  @Return: 
      *  
     */
-    public function checkUserLogin(){
+    public function PassCheck(){
         $this->load->library('session');
         $this->load->model('user_model');
-        
         $clean = array();
-        
-        if ($this->session->userdata('authnum_session') != $this->input->post('loginValidateCode', TRUE)){
-            echo json_encode(array('code' => -1, 'error' => '验证码不正确'));
-            return 0;
-        }
-        
-        if (!$this->input->post('loginMobile', TRUE) || 11 != strlen($this->input->post('loginMobile', TRUE))){
-            echo json_encode(array('code' => -2, 'error' => '抱歉，您的手机号不合法'));
+        if (!$this->input->post('WebUserNO', TRUE) || !ctype_digit($this->input->post('WebUserNO', TRUE))){
+            echo json_encode(array('code' => -1, 'error' => '抱歉，您的教务处账号不合法'));
             return 0;
         } else {
-            $clean['user_telephone'] = $this->input->post('loginMobile', TRUE);
+            $clean['WebUserNO'] = $this->input->post('WebUserNO', TRUE);
         }
         
-        if (!$this->input->post('loginPassword', TRUE) || 20 < strlen($this->input->post('loginPassword', TRUE))){
-            echo json_encode(array('code' => -3, 'error' => '抱歉，您的密码不能超过20个字符或为空'));
-            return 0;
-        } else {
-            $clean['user_password'] = $this->input->post('loginPassword', TRUE);
-        }
+        //从数据库中提取验证是否合法数据
+        //数据库中填写
         
-        $result = $this->user_model->checkPassword($clean['user_telephone'], $clean['user_password']);
         
-        if ($result == FALSE){
-            echo json_encode(array('code' => -4, 'error' => '抱歉，查无此人或密码错误'));
-            return 0;
-        }
+        //cURL请求
+        $url = 'http://jwc.sut.edu.cn/ACTIONLOGON.APPPROCESS?mode=4';
+        $clean['Password'] = $this->input->post('Password', TRUE);
+        $clean['Agnomen'] = $this->input->post('Agnomen', TRUE);
         
+        $ch = curl_init();
+        $postField = 'WebUserNO=' . $clean['WebUserNO'] . '&Password=' . $clean['Password'] . '&Agnomen=' . $clean['Agnomen'];
+        curl_setopt($ch, CURLOPT_URL, $url);
+        $cookie = "JSESSIONID=VIswXYNsHPyLQb1pjgy35hg6huuGJ1agRXI9dy2OrChYFlv1JC6d";//设置cookie值，cookie过期则需要重新写入
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Cookie:' . $this->session->userdata('cookie')));
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postField);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        
+        
+        $result_array = array();
+        $result_array = explode('<td align="left">', $result);
+        $result_array = explode('</td>', $result_array[1]);
+//        echo $result_array[0];
         //写入session
-        $this->session->set_userdata('user_name', $result['user_name']);
-        $this->session->set_userdata('user_role', $result['user_role']);
-        $this->session->set_userdata('user_id', $result['_id']);
-        $this->session->set_userdata('user_telephone', $result['user_telephone']);
-        $this->session->set_userdata('user_school', $result['user_school']);
+        $this->session->set_userdata('user_name', $result_array[0]);
         
-        echo json_encode(array('code' => 1, 'user_name' => $result['user_name']));
-    }
-    
-    
-    
-    /**    
-     *  @Purpose:    
-     *  验证用户注册    
-     *  @Method Name:
-     *  checkUserRegister()    
-     *  @Parameter: 
-     * 
-     * 
-     *  @Return: 
-     *  
-    */
-    public function checkUserRegister(){
-        $this->load->model('user_model');
-        $this->load->library('encrypt');
-        $this->load->library('session');
-        
-        $clean = array();
-        
-        if ($this->session->userdata('authnum_session') != $this->input->post('registerValidateCode', TRUE)){
-            echo json_encode(array('code' => -1, 'error' => '抱歉，您的验证码输入有误'));
-            return 0;
-        }
-        
-        if (!$this->input->post('registerTele', TRUE) || !ctype_digit($this->input->post('registerTele', TRUE)) || 11 != strlen($this->input->post('registerTele', TRUE))){
-            echo json_encode(array('code' => -2, 'error' => '您的手机号需要是11位数字'));
-            return 0;
-        } else {
-            $clean['user_telephone'] = $this->input->post('registerTele', TRUE);
-        }
-        
-        if ($this->user_model->checkUserExist($this->input->post('registerTele', TRUE))){
-            echo json_encode(array('code' => -10, 'error' => '此用户已注册'));
-            return 0;
-        }
-        
-        if (!$this->input->post('registerMail', TRUE) || !filter_var($this->input->post('registerMail', TRUE), FILTER_VALIDATE_EMAIL)){
-            echo json_encode(array('code' => -3, 'error' => '您的邮箱为空或不合法'));
-            return 0;
-        } else {
-            $clean['user_mail'] = $this->input->post('registerMail', TRUE);
-        }
-        
-        if ($this->input->post('registerPassword', TRUE) != $this->input->post('registerPasswordConfirm', TRUE)){
-            echo json_encode(array('code' => -4, 'error' => '两次的密码不正确'));
-            return 0;
-        } else {
-            if (!$this->input->post('registerPassword', TRUE) || 20 < strlen($this->input->post('registerPassword', TRUE))){
-                echo json_encode(array('code' => -5, 'error' => '密码不能为空或超过20个字符'));
-                return 0;
-            } else {
-                $clean['user_password'] = $this->encrypt->encode($this->input->post('registerPassword', TRUE));
-            }
-        }
-        
-        if (!$this->input->post('registerName', TRUE) || 20 < mb_strlen($this->input->post('registerName', TRUE))){
-            echo json_encode(array('code' => -6, 'error' => '您的姓名不能为空或超过20个字符'));
-            return 0;
-        } else {
-            $clean['user_name'] = $this->input->post('registerName', TRUE);
-        }
-        
-        if (!$this->input->post('registerNumber', TRUE) || !ctype_digit($this->input->post('registerNumber', TRUE)) || 20 < strlen($this->input->post('registerNumber', TRUE))){
-            echo json_encode(array('code' => -7, 'error' => '您的学号不能为空或超过20位'));
-            return 0;
-        } else {
-            $clean['user_number'] = $this->input->post('registerNumber', TRUE);
-        }
-        
-        if (!$this->input->post('registerSchool', TRUE) || 20 < mb_strlen($this->input->post('registerSchool', TRUE))){
-            echo json_encode(array('code' => -8, 'error' => '您的学校不能为空或超过20个字符'));
-            return 0;
-        } else {
-            $clean['user_school'] = $this->input->post('registerSchool', TRUE);
-        }
-        
-        
-        if (!$this->input->post('registerMajor', TRUE) || 30 < mb_strlen($this->input->post('registerMajor', TRUE))){
-            echo json_encode(array('code' => -9, 'error' => '您的注册专业班级不能为空或超过30个字符'));
-            return 0;
-        } else {
-            $clean['user_major']  = $this->input->post('registerMajor', TRUE);
-        }
-        
-        $clean['user_role'] = '普通用户';
-        $clean['user_join_time'] = date('Y-m-d H:i:s');
-                
-        //完成数据验证，写入数据库
-        if (is_string($result = $this->user_model->addUser($clean))){
-            echo json_encode(array('code' => -11, 'error' => $result));
-            return 0;
-        } else {
-            //插入成功
-            $this->session->set_userdata('user_role', '普通用户');
-            $this->session->set_userdata('user_telephone', $this->input->post('registerTele', TRUE));
-            $this->session->set_userdata('user_id', $result[1]);
-            $this->session->set_userdata('user_name', $this->input->post('registerName', TRUE));
-            
-            echo json_encode(array('code' => 1));
-            return 0;
-        }
-        
-    }
-    
-    
-    /**    
-     *  @Purpose:    
-     *  用户注销    
-     *  @Method Name:
-     *  logout()    
-     *  @Parameter: 
-     * 
-     * 
-     *  @Return: 
-     *  
-    */
-    public function logout(){
-        $this->load->library('session');
-        
-        if ('1' == $this->input->post('logout', TRUE)){
-            $this->session->sess_destroy();
-            echo 'success';
-        }
-    }
-    
-    
-    
-    /**    
-     *  @Purpose:    
-     *  获取活动信息    
-     *  @Method Name:
-     *  getActInfo()    
-     *  @Parameter: 
-     * 
-     * 
-     *  @Return: 
-     *  
-    */
-    public function getActInfo(){
-        $this->load->library('session');
-        $this->load->library('cache');
-        $this->load->model('act_model');
-        
-        if (!$this->session->userdata('user_id')){
-            echo json_encode(array('code' => -1, 'error' => '请您登录后查看'));
-            return 0;
-        }
-        
-        $mc = $this->cache->memcache();
-        
-        if (!$data = $mc->get('ida_' . $this->cache->getNS('act') . '_act_' . $this->input->post('act_id', TRUE))){
-            if ($result = $this->act_model->getActInfoById($this->input->post('act_id', TRUE))){
-                $mc->set('ida_' . $this->cache->getNS('act') . '_act_' . $this->input->post('act_id', TRUE), $result);
-                echo json_encode(array('code' => 1, 'act_info' => $result));
-            } else {
-                echo json_encode(array('code' => -2, 'error' => '未检索到您的活动'));
-                return 0;
-            }
-        } else {
-            echo json_encode(array('code' => 1, 'act_info' => $data));
-        }
     }
 }
--- application/views/login_view.php
+++ application/views/login_view.php
@@ -2,12 +2,10 @@
 <html lang="zh-cn">
 <head>
 	<meta charset="utf-8">
-	<title>欢迎使用WSPDM2-基于PHP Websocket的数据库管理器</title>
+	<title>欢迎使用德智体综合积分测评平台</title>
         <script src="http://libs.baidu.com/jquery/2.0.0/jquery.min.js"></script>
         <script src="http://libs.baidu.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
         <link href="http://libs.baidu.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
-        <script type="text/javascript" src="<?= base_url('js/swfobject.js')?>"></script>
-        <script type="text/javascript" src="<?= base_url('js/web_socket.js')?>"></script>
         <script type="text/javascript" src="<?= base_url('js/json.js')?>"></script>
         <script type="text/javascript" src="<?= base_url('js/jquery.form.js')?>"></script>
         <style>
@@ -23,88 +21,7 @@
               height: 80px;
             }
         </style>
-        <script>
-        
-        if (typeof console == "undefined") {    this.console = { log: function (msg) {  } };}
-        WEB_SOCKET_SWF_LOCATION = "swf/WebSocketMain.swf";
-        WEB_SOCKET_DEBUG = true;
-        var ws, ping, ping_interval, reconnect_interval, name = 'null', user_list={};        
-        
-        //connect();
-        //function connect(){
-        
-//        try {
-        ws = new WebSocket("ws://"+document.domain+":8080/");
-            
-//        } catch (e){
-//            while (1 != ws.readyState){
-//                ws = new WebSocket("ws://"+document.domain+":8080/");
-//            }
-//        }
-        
-        //}
-        
-        
-        //断线重连
-        /*function reconnect(){
-            if (1 != ws.readyState){
-                alert('yoo');
-                connect();
-            } else {
-                ping_interval = setInterval("getping()",1000);
-                clearInterval(reconnect_interval);
-            }         
-        }*/
-        //显示连接状态
-        function getping(){ 
-            var date = new Date();
-            ping = date.getTime();              
-            ws.send(JSON.stringify({"type":"ping"}));                      
-        }       
-          // 当socket连接打开时，输入用户名
-          ws.onopen = function() {  
-              ping_interval = setInterval("getping()",1000);   
-              $("#db_username, #db_password, #db_type, #db_host, #db_port, .btn").removeAttr("disabled");
-//              console.log(ws.readyState);
-              //ws.send(JSON.stringify({"type":"noajax","name":name}));
-          };
-          
-          // 当有消息时根据消息类型显示不同信息
-          ws.onmessage = function(e) {
-            console.log(e.data);
-            var result = JSON.parse(e.data);  
-            console.log(result);
-            
-            if (result[0] != "1"){
-                switch (result[0])
-                {                
-                    case "p":    //ping
-                        var date = new Date();
-                        ping = date.getTime() - ping;
-                        //alert(date.getTime());
-                        $("#ping").html(ping + "ms");
-                        break;
-                    default:
-                        alert(result[1]);
-                }          
-            } else {
-                location.href='<?= base_url("index.php/ControlCenter")?>';
-            }
-            
-                        
-          };
-          ws.onclose = function() {
-                console.log("服务端关闭了连接");                
-                $("#db_username, #db_password, #db_type, #db_host, #db_port, .btn").attr("disabled", "disabled");
-                alert("和服务器断开连接");
-              //clearInterval(ping_interval);
-              //reconnect_interval = setInterval("reconnect()",1000);              
-          };
-          ws.onerror = function() {
-              console.log("出现错误");              
-          };          
-        </script>    
-        <script>
+<!--        <script>
             var options = {
                 dataType : "json",
                 beforeSubmit : function (){
@@ -116,12 +33,6 @@
                     switch (result[0])
                     {
                         case 1:
-                            if (!$("#ping").html()){
-                                alert('无法连接WebSocket服务器，请使用控制台开启Workerman');
-                                $(".btn").html("登录");
-                                $(".btn").removeAttr("disabled");  
-                                return ;
-                            }
                             location.href='<?= base_url("index.php/ControlCenter")?>';
                             break;
                         default:
@@ -134,41 +45,26 @@
             };
             
             $(".form-signin").ajaxForm(options);  
-        </script>
+        </script>-->
 </head>
 <body>
     <div class="col-sm-8 col-sm-offset-2">
         <form action="<?= base_url("index.php/index/PassCheck")?>" class="form-signin" role="form" method="post">
-        <h2 class="form-signin-heading">欢迎使用WSPDM2</h2>
+        <h2 class="form-signin-heading">欢迎使用德智体综合积分测评平台</h2>
         <br/>
         <div class="form-group">            
-            <input type="text" name="db_username" id="db_username" class="form-control" placeholder="数据库账号" disabled="disabled" required="" autofocus="">
-            <input type="password" name="db_password" id="db_password" class="form-control" placeholder="数据库密码" disabled="disabled">            
-            <select class="form-control" name="db_type" id="db_type" disabled="disabled">
-                <option>MySQL</option>    
-                <option>MongoDB</option>    
-                <option>Oracle</option>    
-                <option>MS SQL Server</option>    
-                <option>MSSQL</option>    
-                <option>ODBC</option>    
-                <option>IBM</option>    
-                <option>cubrid</option>    
-                <option>firebird</option>    
-                <option>INFORMIX</option>    
-                <option>4D</option>    
-                <option>PostgreSQL</option>    
-            </select>
-        <hr>
-        <input type="text" name="db_host" id="db_host" disabled="disabled" class="form-control" placeholder="数据库地址（选填）">
-        <input type="text" name="db_port" id="db_port" disabled="disabled" class="form-control" placeholder="数据库端口（选填）">             
+            <input type="text" name="WebUserNO" id="WebUserNO" class="form-control" placeholder="教务处用户名" required="" autofocus="">
+            <input type="password" name="Password" id="Password" class="form-control" placeholder="教务处密码" required="">            
+            <input type="text" name="Agnomen" id="Agnomen" class="form-control col-lg-2" required="" placeholder="验证码"><img src="<?= base_url("index.php/index/getAgnomen") ?>" width="100">
         </div>
+        
         <br/>
-        <button type="submit" disabled="disabled"  class="btn btn-lg btn-primary btn-block">登录数据库</button>
+        <button type="submit" class="btn btn-lg btn-primary btn-block">登录</button>
         </form>
     </div>
     <div class="footer">
       <div class="container">
-          <p class="text-muted">Ping:<a id="ping"></a><br/>Project WSPDM2<br/>版权所有(C) 2014-<?= date('Y')?> 沈阳工业大学ACM实验室 沈阳工业大学网络管理中心 *Chen<br/>Released under the GPL V3.0 License</p>
+          <p class="text-muted">Project sut_score<br/>版权所有(C) 2015-<?= date('Y')?> 沈阳工业大学ACM实验室 沈阳工业大学网络管理中心 *Chen<br/>Released under the GPL V3.0 License</p>
       </div>
     </div>
 </body>
